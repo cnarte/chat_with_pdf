a36b6381ae66e204fe207cc844b67a3a7c988ce1
cnarte <ankitkrchy.54@gmail.com>
2023-07-16 13:26:59 +0530

    updated

---
+++ Readme.md
@@ -0,0 +1,10 @@
+## First make a env
+    conda create -n "chatPDF"
+ 
+## Install dependency 
+    pip install -r requirement.txt
+
+## Start the sever 
+    uvicorn app:app --reload --port 8008
+
+## Now Run in Postman
---
+++ app.py
@@ -0,0 +1,39 @@
+import os
+from fastapi import FastAPI, UploadFile, File, Request
+from pydantic import BaseModel
+from typing import List
+from embedchain import OpenSourceApp
+import os
+os.environ["OPENAI_API_KEY"] = "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"
+
+
+app = FastAPI()
+naval_chat_bot = OpenSourceApp()
+
+class PDF(BaseModel):
+    file: bytes
+    filename: str
+
+@app.post("/upload_pdf")
+async def upload_pdf(pdf: UploadFile = File(...)):
+    print("file addition")
+    contents = await pdf.read()
+    if pdf.filename.endswith('.pdf'):
+        file_path = os.path.join("files", pdf.filename)
+        with open(file_path, "wb") as f:
+            f.write(contents)
+        naval_chat_bot.add("pdf_file", file_path)
+        return {"message": "PDF file uploaded and added successfully."}
+    else:
+        return {"message": "Only PDF files are allowed."}
+
+
+
+@app.post("/chat")
+async def chat_with_files(req: Request):
+    print("chat")
+    rqs = await req.json()
+    
+    chat_input = rqs["chat_input"]
+    responses = naval_chat_bot.chat(chat_input)
+    return {"chat_putput" : responses}
